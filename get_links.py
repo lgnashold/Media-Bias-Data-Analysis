c1f870d9fbca83c36c902374db475da2004735e0
Anna AD <anna.arpacidusseau@gmail.com>
2019-02-08 21:44:51 -0600

    Link Scraping Works

---
+++ get_links.py
@@ -0,0 +1,13 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+x = urllib.request.urlopen("https://www.allsides.com/topics-issues")
+html_doc = x.read().decode('utf-8')
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+for link in filter(None, soup.find_all('a')):
+    if str(link.get('href')).startswith("/topics/"):
+       print("https://www.allsides.com/topics-issues" + str(link.get('href')))
+
+
